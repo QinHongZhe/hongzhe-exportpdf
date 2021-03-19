0282bb7844cb6301cb70558af342433b8f3b16e0
[tangshuai] <tangshuai318@163.com>
2021-03-19 15:30:04 +0800

    新增TApp测试类

---
+++ src/main/java/com/TApp.java
@@ -0,0 +1,12 @@
+package com;
+
+/**
+ * @author TANGSHUAI
+ * @version 1.0
+ * @date 2021-03-19 15:24
+ */
+public class TApp {
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+}
